3565df447075561bce4ccb49b1e038452256354e
sreddy513 <santoshreddy.spy@live.com>
2024-10-23 16:06:03 +0530

    Update provider.tf

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "myapp_rg_statefile"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "wfd6aiaapiea"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "myblobs"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
+  #backend "azurerm" {
+  #  resource_group_name  = "myapp_rg_statefile"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+  #  storage_account_name = "wfd6aiaapiea"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+  #  container_name       = "myblobs"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+  #  key                  = "terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  #}
 }
 
 provider "azurerm" {
